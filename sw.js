96ed60376c45cefcc8de352fc1aef8d4dd5c1c1d
Riluri-A <ali.e_bn@yahoo.com>
2026-05-26 11:56:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'book-journal-v1';
 const urlsToCache = [
-    '/book-journal/',
-    '/book-journal/index.html',
-    '/book-journal/manifest.json',
-    '/book-journal/icons/icon-192.png',
-    '/book-journal/icons/icon-512.png'
+    '/book-journal-app/',
+    '/book-journal-app/index.html',
+    '/book-journal-app/manifest.json',
+    '/book-journal-app/icons/icon-192.png',
+    '/book-journal-app/icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
